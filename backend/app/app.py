5bb07c9ddde88b8c8c4a757db594410398118baf
Andreas Olsson <olsson.andreas@gmail.com>
2020-06-07 15:43:19 +0200

    Basic CRUD for subreddits

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,10 +1,37 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return {'hello': 'world'}
+subreddits = [
+    {
+        "name": "Formula 1",
+        "url": "formula1"
+    },
+    {
+        "name": "IndyCar",
+        "url": "indycar"
+    }
+]
 
+@app.route('/')
+def get_subreddits():
+    return jsonify(subreddits)
+
+@app.route('/subreddits', methods=["POST"])
+def add_subreddit():
+    subreddit = request.get_json()
+    subreddits.append(subreddit)
+    return {'id': len(subreddits)}, 200
+
+@app.route('/subreddits/<int:index>', methods=['PUT'])
+def update_subreddit(index):
+    subreddit = request.get_json()
+    subreddits[index] = subreddit
+    return jsonify(subreddits[index]), 200
+
+@app.route('/subreddits/<int:index>', methods=['DELETE'])
+def delete_subreddit(index):
+    subreddits.pop(index)
+    return 'None', 200
 
 app.run(host="0.0.0.0", port = 5000,debug=True)
