f862b7c02cb21a5c939b20b58fb3b6c20ffc248f
Andreas Olsson <olsson.andreas@gmail.com>
2020-06-13 17:24:12 +0200

    Threads work

--- backend/app/database/models.py
+++ backend/app/database/models.py
@@ -1,16 +1,19 @@
 from .db import db
 from flask_bcrypt import generate_password_hash, check_password_hash
-
+import datetime
 class Subreddit(db.Document):
     name = db.StringField(required=True, unique=True)
     url = db.StringField(required=True, unique=True)
-    added_by = db.ReferenceField('User')
+    created_by = db.ReferenceField('User')
+    threads = db.SortedListField(db.ReferenceField('Thread'))
 
 class User(db.Document):
     email = db.EmailField(required=False, unique=True)
     username = db.StringField(required=True, unique=True)
     password = db.StringField(required=True, min_length=6)
-    subreddits = db.ListField(db.ReferenceField('Subreddit', reverse_delete_rule=db.PULL))
+    subreddits = db.SortedListField(db.ReferenceField('Subreddit'))
+    threads = db.SortedListField(db.ReferenceField('Thread'))
+    comments = db.SortedListField(db.ReferenceField('Comment'))
 
     def hash_password(self):
         self.password = generate_password_hash(self.password).decode('utf8')
@@ -18,4 +21,18 @@ class User(db.Document):
     def check_password(self, password):
         return check_password_hash(self.password, password)
 
-User.register_delete_rule(Subreddit, 'added_by', db.CASCADE)
+
+class Thread(db.Document):
+    title = db.StringField(required=True)
+    content = db.StringField(required=True)
+    created_by = db.ReferenceField('User')
+    created_at = db.DateTimeField(default=datetime.datetime.now)
+    comments = db.SortedListField(db.ReferenceField('Comment'))
+    subreddit = db.ReferenceField('Subreddit')
+
+class Comment(db.Document):
+    content = db.StringField(required=True)
+    created_by = db.ReferenceField('User')
+    created_at = db.DateTimeField(default=datetime.datetime.now)
+    thread = db.ReferenceField('Thread')
+
