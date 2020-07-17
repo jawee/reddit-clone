8cd8bcd33feb5a556c7e903fc4327343d9023382
Andreas Olsson <olsson.andreas@gmail.com>
2020-07-17 20:02:50 +0200

    Created Login form and added a route for it

--- frontend/react/src/App.js
+++ frontend/react/src/App.js
@@ -3,6 +3,7 @@ import SubredditList from './components/Subreddit-list';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Subreddit from './components/Subreddit';
+import Login from './components/Login';
 import { Route, HashRouter} from 'react-router-dom';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
           <Route exact path="/" component={Home}/>
           <Route path="/subreddits" component={SubredditList}/>
           <Route path="/subreddit/:subredditUrl"><Subreddit /></Route>
+          <Route path="/login"><Login /></Route>
         </div>
       </div>
     </HashRouter>
